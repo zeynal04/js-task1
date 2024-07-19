80a2cd8b78ec657ba52a86416f0781d4ad253b11
zeynal04 <zeynalmehdi123@gmail,com>
2024-07-20 00:25:40 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,199 @@
+
+
+// function task() {}
+
+//         //     task-1 
+
+// let a=0          
+//     if (a < 0) {
+//       console.log("menfi");
+//     }
+//     else if(a > 0){
+// console.log(" musbet");
+//     }
+//      else {
+//       console.log("sifirdir");
+//     }
+
+
+
+//         //      task-2
+
+// var age=prompt(" Yasinizi qeyd edin")
+// switch(age){
+//     case"14":
+//     console.log("bu sayt aileniz yaninizda deyilse size uygun deyil");
+//     break;
+//     case"15":
+//     console.log("bu sayt aileniz yaninizda deyilse size uygun deyil");
+//     break;
+//     case"16":
+//     console.log("aileniz yaninizda deyilse odenis etmeyin");
+//     break;
+//     case"17":
+//     console.log("aileniz yaninizda deyilse odenis etmeyin");
+//     break;
+//     case"18":
+//     console.log("xos geldiniz istediyiniz kimi odenis edin");
+//     break;
+//     case"19":
+//     console.log("xos geldiniz istediyiniz kimi odenis edin");
+//     break;
+//     case"20":
+//     console.log("xos geldiniz istediyiniz kimi odenis edin");
+//     break;
+
+// }
+
+
+//         //      task-3
+
+// let reqem = 0; 
+
+// if (reqem == 0) {
+//     console.log("sifir");
+// } else if (reqem % 2 === 0) {
+//     console.log("cut eded");
+// } else {
+//     console.log("tek eded");
+// }
+
+
+
+
+//         //     task-4
+
+
+// var a=8
+
+// a>0 ? console.log("musbet") : console.log("menfi");
+
+
+//         //     task-5
+
+//             console.log(1 == '1');
+
+//             console.log(undefined==null);
+
+
+//     console.log(1 === '1');
+
+//             console.log(undefined===null);
+
+
+
+
+
+//         //      task-6
+//   eded=10
+
+//   console.log(eded!==10);
+
+
+
+
+
+
+//  task-7
+
+
+
+// //         // task-8
+
+//         let istifadeci = "zeynal";   //buradaki "zeynali deyisib null etsek consolda anonim eks olunacaq"
+//        istifadeci ??( istifadeci= "Anonim ");
+
+//         console.log(istifadeci); 
+
+
+
+//                 //  task-9
+
+// let tehsil=true;
+
+// console.log( !tehsil);
+
+
+
+
+
+
+// //        task-10
+
+// let reqem1=15;
+// let reqem2=6;
+// let reqem3=15;
+// if( reqem1==reqem3 && reqem1>reqem2){console.log('ifade duzgundur');
+
+// }
+// else{
+//       '  ifade yanlisdir'
+// }
+
+
+// //   task-11
+
+// let reqem1=15;
+// let reqem2=6;
+// let reqem3=15;
+// if( reqem1==reqem2 || reqem2 > reqem1){console.log('ifade duzgundur');
+
+// }
+// else{
+//      console.log( '  ifade yanlisdir');
+// }
+
+
+// //         // task-12
+
+
+//     let eded=prompt('reqem qeyd edin')
+
+//     switch(eded){
+
+//         case 0<eded:
+//                 console.log("eded menfidir");
+
+//     }
+
+//     var num =5
+//     if (num >0 && number %2 ==0 ){
+
+//         console.log(musbet);
+//     }else{
+//         console.log(menfi);
+//     }
+
+
+
+//              // task-13
+
+// let username =prompt ('ad');
+// let password= prompt('sifre');
+// if(username=='Zeynal' && password=='123'){console.log('Melumatlar duzgundur. Xos geldiniz');
+
+// }
+// else{
+//         console.log('melumatlari duzgun girdiyinizden emin olun');
+// }
+
+
+
+// // task-14
+
+
+
+// let num = 8;
+// switch (true) {
+//         case (num < 0):
+//                 console.log('menfidir ');
+//                 break;
+//         case (num == 0):
+//                 console.log('sifirdir');
+//                 break;
+//         default:
+//                 console.log('musbetdir');
+//                 break;
+
+
+// }
